1be8fd2e0818f4d527a6eaa6b64acd7d7335ccef
ömer faruk <omerfaruk@transformativeworks.org>
2026-09-07 15:44:58 +0300

    AO3-7444 refactor concern into a class with class methods

--- app/models/concerns/confusable.rb
+++ app/models/concerns/confusable.rb
@@ -3,14 +3,12 @@ module Confusable
 
   # This is stricter than the confusable Unicode defines, it's case-insensitive
   # and also includes dialectics, blank and punctuations.
-  def confusable?(string1, string2)
+  def self.confusable?(string1, string2)
     internal_skeleton(string1).gsub(/\p{Dia}|\p{Blank}|\p{Punct}/, "").upcase ==
       internal_skeleton(string2).gsub(/\p{Dia}|\p{Blank}|\p{Punct}/, "").upcase ||
       internal_skeleton(string1.upcase).gsub(/\p{Dia}|\p{Blank}|\p{Punct}/, "").upcase ==
         internal_skeleton(string2.upcase).gsub(/\p{Dia}|\p{Blank}|\p{Punct}/, "").upcase
   end
-
-  private
 
   # Internal skeleton that Unicode defines in Technical Standard #39
   #
@@ -24,7 +22,7 @@ module Confusable
   # character, join each of those arrays (or codepoint of source character if
   # it's not confusable) after converting them to a string. Also normalize
   # before and after as described by the standard.
-  def internal_skeleton(string)
+  def self.internal_skeleton(string)
     confusable_pairs = Rails.cache.fetch("v1/confusables_hash") do
       File.foreach(Rails.root.join("config/confusables.txt"))
         .filter_map { it.match(/^(\h+)\s+;\s+([\h\s]+);\s*MA/) }
@@ -35,4 +33,6 @@ module Confusable
       (confusable_pairs[codepoint] || [codepoint]).pack("U*")
     end.join.unicode_normalize(:nfd)
   end
+
+  private_class_method :internal_skeleton
 end
--- app/validators/not_forbidden_name_validator.rb
+++ app/validators/not_forbidden_name_validator.rb
@@ -1,10 +1,9 @@
 class NotForbiddenNameValidator < ActiveModel::EachValidator
-  include Confusable
   def validate_each(record, attribute, value)
     return if value.nil?
 
     ArchiveConfig.FORBIDDEN_USERNAMES.each do |forbidden|
-      if confusable?(forbidden, value)
+      if Confusable.confusable?(forbidden, value)
         # i18n-tasks-use t("activerecord.errors.messages.forbidden")
         return record.errors.add(attribute, :forbidden, **options.merge(value: value))
       end
